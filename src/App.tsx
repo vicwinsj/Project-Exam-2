b000ce568af08463576faa1e810e6471ead402c5
Victor Winsjansen <victor.winsjansen@icloud.com>
2025-04-25 12:03:50 +0700

    Added temporary header

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import Layout from "./components/Layout";
+import HomeView from "./views/HomeView";
+// import Register from "./views/Register";
+// import Venue from "./views/Venue";
+// import Profile from "./views/Profile";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="bg-primary">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomeView />} />
+          {/* <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/venue/:venueId" element={<Venue />} /> */}
+        </Routes>
+      </Layout>
+    </Router>
   )
-}
+};
 
-export default App
+export default App;
