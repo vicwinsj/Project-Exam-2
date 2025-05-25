f8923ebc26b6bb54e3b59e4e839de6ea3a2ebe68
Victor Winsjansen <victor.winsjansen@icloud.com>
2025-05-25 15:45:15 +0700

    Added numbers

--- src/components/modals/ImageCarousel.tsx
+++ src/components/modals/ImageCarousel.tsx
@@ -33,7 +33,7 @@ export const ImageCarousel = ({
 
   return (
     <ModalWrapper onClose={onClose} isImageCarousel={true}>
-      <div className="w-full h-1/2 sm:h-full relative">
+      <div className="flex flex-col items-center w-full h-1/2 md:h-full relative">
         {/* Image */}
         <img
           src={images[currentIndex].url}
@@ -41,10 +41,18 @@ export const ImageCarousel = ({
           className="w-full h-full object-cover"
         />
 
+        {/* Numbers */}
+        <p
+          onClick={onClose}
+          className="text-white/70 text-sm font-semibold flex items-center absolute right-6 -bottom-7"
+        >
+          {currentIndex + 1}/{images.length}
+        </p>
+
         {/* Exit Button */}
         <button
           onClick={onClose}
-          className="transition-colors duration-300 flex items-center justify-center absolute right-4 -top-33 sm:top-9 transform -translate-y-1/2 text-white/70 hover:text-white size-10 rounded-full p-3 "
+          className="bg-black/50 transition-colors duration-300 flex items-center justify-center absolute right-4 -top-30 md:top-4 text-white/70 hover:text-white size-8 rounded-full p-3"
         >
           <FontAwesomeIcon size="lg" icon={faXmark} />
         </button>
@@ -54,21 +62,21 @@ export const ImageCarousel = ({
             {/* Left Arrow */}
             <button
               onClick={prevImage}
-              className="transition-colors duration-300 flex items-center justify-center absolute left-5 top-1/2 transform -translate-y-1/2 text-white/70 hover:text-white size-10 rounded-full p-3 "
+              className="bg-sunset-800/70 transition-colors duration-300 flex items-center justify-center absolute left-5 top-1/2 transform -translate-y-1/2 text-white/70 hover:text-white size-8 rounded-full p-3 "
             >
-              <FontAwesomeIcon icon={faChevronLeft} size="lg" />
+              <FontAwesomeIcon icon={faChevronLeft} />
             </button>
 
             {/* Right Arrow */}
             <button
               onClick={nextImage}
-              className="transition-colors duration-300 flex items-center justify-center absolute right-5 top-1/2 transform -translate-y-1/2 text-white/70 hover:text-white size-10 rounded-full p-3"
+              className="bg-sunset-800/70 transition-colors duration-300 flex items-center justify-center absolute right-5 top-1/2 transform -translate-y-1/2 text-white/70 hover:text-white size-8 rounded-full p-3"
             >
-              <FontAwesomeIcon icon={faChevronRight} size="lg" />
+              <FontAwesomeIcon icon={faChevronRight} />
             </button>
 
             {/* Dots */}
-            <div className="transition-all duration-300 flex items-center absolute bottom-4 gap-2 justify-center w-full">
+            <div className="bg-black/50 rounded-full px-2 py-1 transition-all duration-300 flex items-center absolute bottom-4 gap-2 justify-center w-fit">
               {images.map((_, index) => (
                 <div
                   key={index}
