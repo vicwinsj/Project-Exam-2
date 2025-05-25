3e7ef44d0e24de7dc8a80f18a603868f4b9f42c6
Victor Winsjansen <victor.winsjansen@icloud.com>
2025-05-25 22:54:50 +0700

    Built for publish

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Layout from "./components/layout/Layout";
 import HomeView from "./views/HomeView";
 import RegisterView from "./views/RegisterView";
